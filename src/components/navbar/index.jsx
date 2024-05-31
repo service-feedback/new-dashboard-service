c4ff64c1d7cefcb500f674229d8975c65f4dcc0b
veronica17-web <veronicamiryala001@gmail.com>
2024-05-31 12:28:56 +0530

    added the statistics page

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -45,6 +45,7 @@ const Navbar = (props) => {
           </Link>
         </p>
       </div>
+      
 
       <div className="relative mt-[3px] flex h-[61px] w-[135px] flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-[145px] md:flex-grow-0 md:gap-1 xl:w-[145px] xl:gap-2">
         <span
